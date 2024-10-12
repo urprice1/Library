59e535c21128af6edcc4a07e3d1f2aaf748e1e1f
urprice1 <urprice1@gmail.com>
2024-10-12 17:25:56 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -30,10 +30,10 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(36, 36, 36);
-    BackgroundColor = Color3.fromRGB(28, 28, 28);
-    AccentColor = Color3.fromRGB(219, 68, 103);
-    OutlineColor = Color3.fromRGB(55, 55, 55);
+    MainColor = Color3.fromRGB(0, 0, 0);
+    BackgroundColor = Color3.fromRGB(0, 0, 0);
+    AccentColor = Color3.fromRGB(147, 0, 211);
+    OutlineColor = Color3.fromRGB(1, 1, 1);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
