e48605d696da48508d62720071d0861ab666ce46
urprice1 <urprice1@gmail.com>
2024-10-13 08:48:04 +0300

    Update ThemeManager.lua

--- Addons/ThemeManager.lua
+++ Addons/ThemeManager.lua
@@ -5,11 +5,11 @@ local ThemeManager = {} do
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
 		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"000000","AccentColor":"9400d3","BackgroundColor":"000000","OutlineColor":"010101"}') },
-		['Red Line'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
-		['Pinky'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"6b00b8","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
-		['Mint'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"3db488","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
-		['Tokyo Night'] 	= { 5, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
-		['Quartz'] 			= { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232330","AccentColor":"426e87","BackgroundColor":"1d1b26","OutlineColor":"27232f"}') },
+		['Red Line'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"000000","AccentColor":"db4467","BackgroundColor":"000000","OutlineColor":"010101"}') },
+		['Pinky'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"000000","AccentColor":"ffc0cb","BackgroundColor":"000000","OutlineColor":"010101"}') },
+		['Mint'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"000000","AccentColor":"3db488","BackgroundColor":"000000","OutlineColor":"010101"}') },
+		['Tokyo Night'] 	= { 5, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"000000","AccentColor":"6759b3","BackgroundColor":"000000","OutlineColor":"010101"}') },
+		['Quartz'] 			= { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"000000","AccentColor":"426e87","BackgroundColor":"000000","OutlineColor":"010101"}') },
 	}
 
 	function ThemeManager:ApplyTheme(theme)
